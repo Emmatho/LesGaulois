c24a504b65e673d1ca7ad0aa99c61538308f5f1e
THM4879A <THM4879A@u3-215-15.ens.fsi>
2023-03-07 17:39:35 +0100

    Création de ajouterHabitant, trouverHabitant et du main dans Village (jusqu'au e du TP2)

--- src/personnages/Village.java
+++ src/personnages/Village.java
@@ -9,13 +9,39 @@ public class Village {
 	
 	public Village(String nom, int nbVillageoisMaximum) {
 		this.nom = nom;
+		this.villageois = new Gaulois[nbVillageoisMaximum];
 	}
 	
-		public void setChef(Chef chef) {
+	public void setChef(Chef chef) {
 			this.chef = chef;
 	}
 		
-		public String getNom() {
+	public String getNom() {
 			return nom;
+	}
+	
+	public void ajouterHabitant(Gaulois gaul) {
+		villageois[nbVillageois] = gaul;
+		nbVillageois = nbVillageois + 1;
+	}
+	
+	public Gaulois trouverHabitant(int num) {
+		return villageois[num];
+	}
+	
+	public static void main(String[] args) {
+		Village village;
+		village = new Village("Village des Irréductibles", 30);
+		//Gaulois gaulois = village.trouverHabitant(30);
+		//Le dernier index est 29
+		Gaulois abra = new Gaulois("Abraracourcix", 6);
+		village.ajouterHabitant(abra);
+		Chef abrara = new Chef("Abraracourcix", 6, village);
+		village.setChef(abrara);
+		Gaulois asterix = new Gaulois("Astérix", 8);
+		village.ajouterHabitant(asterix);
+		//Gaulois gaulois = village.trouverHabitant(1);
+		//System.out.println(gaulois);
+		//Le premier habitant est d'indice 0
 	}
 }
