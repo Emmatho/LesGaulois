1672e21a181ff99402eec58458d60d5e4beb9a66
THM4879A <THM4879A@u3-213-04.ens.fsi>
2023-03-28 16:46:50 +0200

    invariant pre post conditions

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -7,6 +7,7 @@ public class Romain {
 	
 	public Romain(String nom, int force) {
 		super();
+		assert force > 0 : "la force d’un Romain est toujours positive";
 		this.nom = nom;
 		this.force = force;
 	}
@@ -24,11 +25,18 @@ public class Romain {
 	}
 
 	public void recevoirCoup(int forceCoup) {
+		assert force < 0 : "La force d’un Romain est positive";
+		int aux = force;
 		force -= forceCoup;
 		if (force > 0) {
 			parler("Aïe");
 		} else {
 			parler("J'abandonne...");
 		}
+		assert aux > force : "La force d'un Romain a diminué";
+	}
+	
+	public static void main(String[] args) {
+		Romain minus = new Romain("Minus", 6);
 	}
 }
