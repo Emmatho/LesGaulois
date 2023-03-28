9badbacebde9575fba15fe7f8cb7ca9bcef6a88b
THM4879A <THM4879A@u3-213-04.ens.fsi>
2023-03-28 17:46:42 +0200

    les énumérés

---
+++ src/personnages/Equipement.java
@@ -0,0 +1,21 @@
+package personnages;
+
+public enum Equipement {
+	
+	CASQUE("casque"), BOUCLIER("bouclier");
+	private String nom;
+	
+	private Equipement(String nom) {
+		this.nom = nom;
+	}
+	
+	@Override
+	public String toString() {
+		return nom;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(CASQUE);
+		System.out.println(BOUCLIER);
+	}
+}
--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -1,21 +1,23 @@
 package personnages;
 
 public class Romain {
-	
+
 	private String nom;
 	private int force;
-	
+	private Equipement[] equipement = new Equipement[2];
+	private int nbEquipement = 0;
+
 	public Romain(String nom, int force) {
 		super();
 		assert force > 0 : "la force d’un Romain est toujours positive";
 		this.nom = nom;
 		this.force = force;
 	}
-	
+
 	public String getNom() {
 		return nom;
 	}
-	
+
 	public void parler(String texte) {
 		System.out.println(prendreParole() + "«" + texte + "»");
 	}
@@ -35,8 +37,40 @@ public class Romain {
 		}
 		assert aux > force : "La force d'un Romain a diminué";
 	}
-	
+
+	private void ajouter(String texte, Equipement eq) {
+		equipement[nbEquipement] = eq;
+		nbEquipement += 1;
+		System.out.println(texte + " s'équipe avec un " + eq);
+	}
+
+	public void sEquiper(Equipement eq) {
+
+		String soldat = "Le soldat " + getNom();
+		switch (nbEquipement) {
+		case 2:
+			System.out.println(soldat + " est déjà bien protégé !");
+			break;
+		case 1:
+			if (equipement[0] == eq) {
+				System.out.println(soldat + " possède déjà un " + eq);
+			} else {
+				ajouter(soldat, eq);
+			}
+			break;
+
+		case 0:
+			ajouter(soldat, eq);
+
+			break;
+		}
+	}
+
 	public static void main(String[] args) {
 		Romain minus = new Romain("Minus", 6);
+		minus.sEquiper(Equipement.CASQUE);
+		minus.sEquiper(Equipement.CASQUE);
+		minus.sEquiper(Equipement.BOUCLIER);
+		minus.sEquiper(Equipement.CASQUE);
 	}
 }
